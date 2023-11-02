87c37600ac9ef4514be81882303d8e021c32ee19
Snr Roko <80484832+snr-roko@users.noreply.github.com>
2023-11-02 11:09:20 +0000

    Simple Html boilerplate and program Steps The simple HTML boilerplate with nav, header, main, and footer tags together with the script tags in the body element of the html, and at the head tag, webpage is configured to be both responsive and support social media links

---
+++ script.js
@@ -0,0 +1,21 @@
+/* The Ingredients
+ Rock
+ Paper
+ Scissors
+*/
+
+// The computer and the user...
+
+// The computer makes its choice among the three 
+
+/* The user also makes its choice among the three
+    Ask the user for his choice */
+
+/* If Rock meets Paper,
+    Paper wins */
+
+/* If Rock meets Scissors,
+    Rock wins */
+
+/* If Paper meets Scissors,
+    Scissors wins */
