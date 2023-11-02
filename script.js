79d9b2eff49b1fe32c6a404ed8fd8ae83efa9275
Snr Roko <80484832+snr-roko@users.noreply.github.com>
2023-11-02 12:15:55 +0000

    Choice ingredients and computer choice completed The choice ingredients which comprises of the rock, paper, and scissors have been stored as both individual variables and an array; one will be chosen afterwards; Also the first function of the game to get the choice of the computer has been written; A variable to get an random number in between 0 and 2 was created and then passed as an index to get a choice

--- script.js
+++ script.js
@@ -3,11 +3,23 @@
  Paper
  Scissors
 */
-
+/* Would it be better to use different variables for all three?
+    Or would it be better to use an array? */    
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
+    const choices = ["rock", "paper", "scissors"];
 // The computer and the user...
 
 // The computer makes its choice among the three 
+    getComputerChoice = () => {
+        // get an index
+        const choice_index = Math.floor(Math.random() * 3);
+        // get a choice
+        const computer_choice = choices[choice_index];
 
+        return computer_choice;
+    };
 /* The user also makes its choice among the three
     Ask the user for his choice */
 
