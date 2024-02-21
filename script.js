8f53597ddec8329a85f808d5f746516e4b62cb10
mr-r <rbbagyei@gmail.com>
2024-02-21 05:19:24 +0000

    Game UI completed The game function has been removed, buttons that were of text are now replaced with svg's of the text. Game plays in five rounds and needs a reload to start again. A mechanism to stop game after 5 rounds have not yet been implemented.

--- script.js
+++ script.js
@@ -5,14 +5,29 @@
 */
 /* Would it be better to use different variables for all three?
     Or would it be better to use an array? */    
-const rock = "rock";
-const paper = "paper";
-const scissors = "scissors";
+
     const choices = ["rock", "paper", "scissors"];
 // The computer and the user...
 
+// Html Variables
+const rock_btn = document.querySelector('#rock-button');
+const paper_btn = document.querySelector('#paper-button');
+const scissors_btn = document.querySelector('#scissors-button');
+let user_score = document.querySelector('#user-score div');
+let ai_score = document.querySelector('#ai-score div')
+let start_round = document.querySelector('#start-round');
+let end_round = document.querySelector('#end-round');
+let game = document.querySelector('#game');
+let reload = document.querySelector('#reload');
+let remarks = document.querySelector('#remarks');
+
+score_user = 0;    
+score_ai = 0;
+
+
+
 // The computer makes its choice among the three 
-    getComputerChoice = () => {
+    let getComputerChoice = () => {
         // get an index
         const choice_index = Math.floor(Math.random() * 3);
         // get a choice
@@ -21,81 +36,73 @@ const scissors = "scissors";
         return computer_choice;
     };
 
-    const rock_btn = document.querySelector('#rock-button');
-    const paper_btn = document.querySelector('#paper-button');
-    const scissors_btn = document.querySelector('#scissors-button');
-    let user_score = document.querySelector('#user-score div');
-    let ai_score = document.querySelector('#ai-score div')
-    let rounds = document.querySelector('#rounds');
 
 
-// function to check whether who wins a round...
-    const game_winner = (player, computer) => {
-        /* The user also makes its choice among the three
-        Ask the user for his choice */
-        
-        computer = getComputerChoice();
-        let winner;
-        switch(true) {
-            /* If rock meets Paper
-            Paper wins */
-            case player == "rock" && computer == "paper":
-                winner = "You lose! Paper beats Rock"
-                break;
-            case player == "paper" && computer == "rock":
-                winner = "You win! Paper beats Rock"
-                break;
-            /* If Rock meets Scissors,
-            Rock wins */
-            case player == "rock" && computer == "scissors":
-                winner = "You win! Rock beats Scissors"
-                break;
-            case player == "scissors" && computer == "rock":
-                winner = "You lose! Rock beats Scissors"
-                break;
-            /* If Paper meets Scissors,
-            Scissors wins */
-            case player == "paper" && computer == "scissors":
-                winner = "You lose! Scissors beats paper"
-                break;
-            case player == "scissors" && computer == "paper":
-                winner = "You win! Scissors beats paper"
-                break;
-            // If same meet
-            default:
-                winner = "It's a tie!"
-        }
-        rounds.textContent = winner;
-        return winner;
+// function to check who wins a round...
+const game_winner = (event) => {
+/* The user also makes its choice among the three
+Ask the user for his choice */
+
+let computer = getComputerChoice();
+let player =  event.target.className;
+let winner;
+switch(true) {
+    /* If rock meets Paper
+    Paper wins */
+    case player == "rock" && computer == "paper":
+        winner = "You lose! Paper beats Rock"
+        break;
+    case player == "paper" && computer == "rock":
+        winner = "You win! Paper beats Rock"
+        break;
+    /* If Rock meets Scissors,
+    Rock wins */
+    case player == "rock" && computer == "scissors":
+        winner = "You win! Rock beats Scissors"
+        break;
+    case player == "scissors" && computer == "rock":
+        winner = "You lose! Rock beats Scissors"
+        break;
+    /* If Paper meets Scissors,
+    Scissors wins */
+    case player == "paper" && computer == "scissors":
+        winner = "You lose! Scissors beats paper"
+        break;
+    case player == "scissors" && computer == "paper":
+        winner = "You win! Scissors beats paper"
+        break;
+    // If same meet
+    default:
+        winner = "It's a tie!"
+}
+end_round.textContent = winner;
+
+if (winner.charAt(4) == "w"){
+    score_user += 1;
+    
+}
+else if (winner.charAt(4) == "l") { 
+    score_ai +=1;
+}
+ai_score.textContent = score_ai;
+user_score.textContent = score_user;
+
+if(score_ai == 5 || score_user == 5) {
+    end_round.textContent = "GAME OVER";
+    reload.textContent = "Reload to Start a new game...";
+    if(score_ai > score_user) {
+        game.textContent = "YOU LOSE!  "
+        remarks.textContent = "Don't go playing Poker please, Your luck is to blame..."
+    } 
+    if(score_user > score_ai) {
+        game.textContent = "YOU WIN!!!  "
+        remarks.textContent = "You must be happy... You didn't do anything though, hahaha!"
     }
+}
+};
 
 
-
-
-
-    // function to create a five round loop 
-    const game = () => {     
-        let player_score = 0;
-        let computer_score = 0;
-        user_score.textContent = player_score;
-        ai_score.textContent = computer_score;
-            if (round_result.charAt(4) == "w"){
-                player_score += 1;
-            }
-            else if (round_result.charAt(4) == "l") { 
-                computer_score +=1
-            }
-            console.log(round_result);
-            console.log(`${player_score} : ${computer_score}`)
-        }
-
-        if (computer_score > player_score) {
-            console.log("GAME OVER, YOU LOSE!")
-        }
-        else if (computer_score == player_score) {
-            console.log("GAME OVER, YOU TIED! PLAY AGAIN")
-        }
-        else {
-            console.log(`GAME OVER, YOU WIN!`)
-        }
+rock_btn.addEventListener('click', game_winner);
+paper_btn.addEventListener('click', game_winner);
+scissors_btn.addEventListener('click', game_winner);
 
